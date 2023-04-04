a0d9002fd734aa34213f93be2f17fdffa60939ed
Errno-404 <adamkmezydlo@gmail.com>
2023-04-04 20:13:39 +0200

    Hope I have finished ex 8 well ..

--- ex.8.sql
+++ ex.8.sql
@@ -52,3 +52,89 @@ begin
 
 
 end;
+
+create or replace trigger StatusChangeControl
+    before update of status
+    on reservation
+    for each row
+
+declare
+    pragma autonomous_transaction ;
+    available_places int;
+begin
+    dbms_output.PUT_LINE('working');
+
+    case
+        when :old.status = 'C' then if :new.status = 'P' then
+            raise_application_error(-20003, 'Error');
+                                    end if;
+
+                                    available_places := CHECKAVAILABLEPLACES(:old.TRIP_ID);
+                                    if available_places = 0 then
+                                        raise_application_error(-20003, 'dont');
+                                    end if;
+
+        when :old.status = 'P' then if :new.status = 'N' then
+            raise_application_error(-20003, 'Error');
+        end if;
+        else null;
+        end case;
+end ;
+
+create or replace procedure ModifyReservationStatus8_0(reservation_id number, status reservation.status%type)
+as
+    trip_id            int;
+    old_status         reservation.status%type;
+    reservation_exists number;
+    is_future          number;
+begin
+
+    -- checking if reservation exists
+    select COUNT(*)
+    into reservation_exists
+    from Reservations r
+    where r.reservation_id = ModifyReservationStatus8_0.reservation_id;
+    if reservation_exists = 0 then
+        RAISE_APPLICATION_ERROR(-20005, 'Reservation with chosen ID does not exist!');
+    end if;
+
+    -- checking if given new status is correct
+    if ModifyReservationStatus8_0.status not in ('N', 'P', 'C') then
+        raise_application_error(-20005, 'Wrong status!');
+    end if;
+
+
+    select r.trip_id
+    into trip_id
+    from Reservation r
+    where r.RESERVATION_ID = ModifyReservationStatus8_0.reservation_id;
+
+    -- checking if trip has already started
+    select COUNT(*)
+    into is_future
+    from FUTURETRIPS ft
+    where ft.trip_id = TRIP_ID;
+
+    if is_future = 0 then
+        RAISE_APPLICATION_ERROR(-20005, 'The trip for which given reservation was made has already started!');
+    end if;
+
+    select r.status
+    into old_status
+    from reservation r
+    where r.RESERVATION_ID = ModifyReservationStatus8_0.reservation_id;
+    if ModifyReservationStatus8_0.status = old_status then
+        raise_application_error(-20003, 'Given reservation already has such status!');
+    end if;
+
+
+
+
+    update RESERVATION
+    set STATUS = ModifyReservationStatus8_0.status
+    where RESERVATION_ID = ModifyReservationStatus8_0.reservation_id;
+
+
+end ;
+
+
